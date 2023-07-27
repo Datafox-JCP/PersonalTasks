2705b0825717b81d4aafcc70363aa16de10c7de6
Datafox-JCP <datafox@outlook.com>
2023-07-26 21:04:33 -0600

    - Dagger-Hilt

---
+++ app/src/main/java/mx/datafox/personaltasks/data/TaskRepository.kt
@@ -0,0 +1,14 @@
+package mx.datafox.personaltasks.data
+
+import kotlinx.coroutines.flow.Flow
+
+interface TaskRepository {
+
+    suspend fun insertTask(task: Task)
+
+    suspend fun deleteTask(task: Task)
+
+    suspend fun getTaskById(id: Int): Task?
+
+    fun getTasks(): Flow<List<Task>>
+}
